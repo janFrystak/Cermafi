112aff13749267364555dbcea72b80f1b836f37d
janFrystak <janfrystak@gmail.com>
2025-11-25 17:59:03 +0100

    api for single id

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -2,28 +2,39 @@ import 'reflect-metadata';
 import {Request, Response} from "express";
 import express from "express";
 import helmet from 'helmet';
-
-const app = express();
 import { AppDataSource } from "./data-source";
 import { Uchazec } from "./Models/Uchazec-model";
 
+const app = express();
 
-//app.use(helmet());
 
+
+app.use(helmet());
+app.use(express.json());
 
 AppDataSource.initialize()
     .then(() =>{
         const uchazecRepository = AppDataSource.getRepository(Uchazec);
+
         app.listen(8080, ()=>{
-            console.log("Listening")
+            console.log("Listening on localhost:8080")
             
         });
 
-        app.get("/:id", async(req:Request, res:Response)=>{
+        app.get("/api/uchazec-single/:id", async(req:Request, res:Response)=>{
             const id = parseInt(req.params["id"]);
-            const uchazec = await uchazecRepository.findOneBy({id});
-       
-            console.log(uchazec);
+            try {
+                const uchazec = await uchazecRepository.findOneBy({id});
+                if (!uchazec) {
+                    return res.status(404).json({message: "Uchazec not found"});
+                }
+                res.json(uchazec);
+                console.log(uchazec);
+            } catch (err) {
+                console.error(err);
+                res.status(500).json({message: "Internal server error"});
+            }
+            
         });
 
         
