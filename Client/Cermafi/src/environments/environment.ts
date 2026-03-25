5d17a66cd76ba9890d81d37e6f94855213e23800
janFrystak <janfrystk@gmail.com>
2026-03-26 00:10:48 +0100

    small url fix

--- Client/Cermafi/src/environments/environment.ts
+++ Client/Cermafi/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   web_url: "https://cermafi.up.railway.app",
-  db_url: "https://" + process.env['API_URL']
+  db_url: process.env['API_URL']
   
 };
