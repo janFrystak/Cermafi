36ef8aa0e6b9991f123719da4b78a6ed9780c361
janFrystak <janfrystk@gmail.com>
2026-03-26 00:57:40 +0100

    surely this fix

--- Client/Cermafi/src/environments/environment.prod.ts
+++ Client/Cermafi/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   web_url: "https://cermafi.up.railway.app",
-  db_url: "https://cermafi-back.railway.internal"
+  db_url: "cermafi-backend-production.up.railway.app"
 };
