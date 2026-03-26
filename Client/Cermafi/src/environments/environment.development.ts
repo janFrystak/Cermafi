a17f72d827aac1199d0b72b84a0d7b9346447746
janFrystak <janfrystk@gmail.com>
2026-03-26 01:29:07 +0100

    small change

--- Client/Cermafi/src/environments/environment.development.ts
+++ Client/Cermafi/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   web_url: "https://cermafi.up.railway.app",
   db_url: "https://cermafi-backend-production.up.railway.app"
 };
