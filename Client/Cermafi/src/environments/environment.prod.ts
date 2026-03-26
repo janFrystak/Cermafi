77c752cb8e31c4319759a7d6a06c77abf019f17b
janFrystak <janfrystk@gmail.com>
2026-03-26 14:04:34 +0100

    possible routing fix

--- Client/Cermafi/src/environments/environment.prod.ts
+++ Client/Cermafi/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  web_url: "https://cermafi-frontend.up.railway.app",
+  web_url: "https://cermafi.up.railway.app",
   db_url: "https://cermafi-backend-production.up.railway.app" //sadly has to be hardcoded
   
 };
